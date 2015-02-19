02247047efd68c30ce9eb04c0cea865760805d67
Ilya Ovdin <iovdin@gmail.com>
2015-02-19 15:43:33 +0300

    show username in all user list

--- web/admin.js
+++ web/admin.js
@@ -1,10 +1,10 @@
 if (Meteor.isServer){
     Router.route("/allusers", function(){
         var emails = Meteor.users.find({}, { fields : {username : 1, "emails.address" : 1 }}).map(function(user){
-            return user.emails[0].address;
+            return [user.username, user.emails[0].address]
         });
         var text = _.chain(emails).map(function(email, index){
-            return (index+1) + ". " + email;
+            return (index+1) + ". " + email[0] + "@wrte.io -> " + email[1];
         }).value().join("\n");
         var allusersTemplate = _.template(Assets.getText("email_templates/allusers.txt"));
         var content = allusersTemplate({emails : text})
